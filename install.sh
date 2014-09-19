ea0ea4f778412b569a5871c9a84a0f5217f47d25
Olen Davis <spelledlikeitsounds@gmail.com>
2014-09-19 13:43:12 -0700

    fix(build): Fix the deleting of the prior version of the renamed source file

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-rm -f js/vendor/001_angular.tail.js
+rm -f js/vendor/002_angular-animate.tail.js
 
 cp -fv bower_components/angular-animate/angular-animate.js js/vendor/002_angular-animate.tail.js
